9b7693cda3f763f4c9cfde50cc16e04e3a7743bf
Thennal <thennal10@gmail.com>
2019-05-20 12:56:29 +0530

    updated bahaha leaderboard command

--- bot.py
+++ bot.py
@@ -1,7 +1,9 @@
 import discord
 
+
 def baha_sort(l):
     return l[1]
+
 
 client = discord.Client()
 print("Running!")
@@ -14,13 +16,9 @@ async def on_message(message):
         return
 
     serv = message.guild
-    #if message.content == "server_stats":
-    #    chanl = serv.by_category()
-    #    for i in range(len(chanl)):
-    #        if chanl[i][0].id == 469563734566633473:
-    #            await message.channel.send(f"""Number of rewatches: {len(chanl[i][1])}""")
-    if message.content.startswith("peachlator"):
-        #load dictionary
+
+    if message.content.startswith("peachlator:"):
+        # load dictionary
         trans_dict = {}
         file = open("peachdict.txt", "r")
         for line in file:
@@ -37,7 +35,7 @@ async def on_message(message):
                     key += word + " "
             trans_dict[key.rstrip()] = value.rstrip()
 
-        #replace words
+        # replace words
         new_message = message.content.split()
         new_message[0] = "Translation:"
         for i in range(len(new_message)):
@@ -59,10 +57,10 @@ async def on_message(message):
                         del new_message[i + l + len(splitval)]
                     i = i - len(splitval)
 
-        #send result
+        # send result
         new_message = " ".join(new_message)
         await message.channel.send(new_message)
-    elif message.content == "bahaha_scoreboard":
+    elif message.content == "bahaha_leaderboard":
         # redo this with pandas
         await message.channel.send("Loading...")
         peeps = [["N/A", 0]]
@@ -81,27 +79,11 @@ async def on_message(message):
                         if found == 0:
                             peeps.append([msg.author.name, 0])
         peeps.sort(key=baha_sort, reverse=True)
-        while len(peeps) < 10:
-            peeps.append(["N/A", 0])
-        print(peeps)
-        await message.channel.send(f"""
-
-**The Bahaha Leaderboard**
-```
-Rank  | Name
-
-[1]   > {peeps[0][0]}: {peeps[0][1]}
-[2]   > {peeps[1][0]}: {peeps[1][1]}
-[3]   > {peeps[2][0]}: {peeps[2][1]}
-[4]   > {peeps[3][0]}: {peeps[3][1]}
-[5]   > {peeps[4][0]}: {peeps[4][1]}
-[6]   > {peeps[5][0]}: {peeps[5][1]}
-[7]   > {peeps[6][0]}: {peeps[6][1]}
-[8]   > {peeps[7][0]}: {peeps[7][1]}
-[9]   > {peeps[8][0]}: {peeps[8][1]}
-[10]  > {peeps[9][0]}: {peeps[9][1]}
-```
-      """)
+        leaderboard = "**The Bahaha Leaderboard**```\nRank  | Name\n\n"
+        for i in range(len(peeps)):
+            leaderboard += f"""[{i + 1}]   > {peeps[i][0]}: {peeps[i][1]}\n"""
+        leaderboard += "```"
+        await message.channel.send(leaderboard)
 
 
 # keep_alive()
