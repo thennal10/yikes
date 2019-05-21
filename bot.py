7736807dcb124589a975355010bd088b565e41e7
Thennal <thennal10@gmail.com>
2019-05-21 11:36:36 +0530

    goddamnit

--- bot.py
+++ bot.py
@@ -99,5 +99,5 @@ async def on_message(message):
             await message.channel.send("Usage: ``bahaha_leaderboard [no. of messages]``")
 
 
-token =  os.environ.get("TOKEN")
+token = os.environ.get("TOKEN")
 client.run(token)
