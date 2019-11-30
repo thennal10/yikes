ab2efa7ed9077e1211e26f62b67fd0876d4ce681
Thennal <thennal10@gmail.com>
2019-11-30 08:37:19 +0530

    changed command prefix

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ TOKEN = os.environ.get("TOKEN")
 def get_prefix(bot, message):
     """A callable Prefix for our bot. This could be edited to allow per server prefixes."""
 
-    prefixes = ['!']
+    prefixes = ['$', 'y!']
 
     # If we are in a guild, we allow for the user to mention us or use any of the prefixes in our list.
     return commands.when_mentioned_or(*prefixes)(bot, message)
