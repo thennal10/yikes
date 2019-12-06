8d628c31483f8985dd338c7fb2007badbbb3c02d
Thennal <thennal10@gmail.com>
2019-12-06 10:09:46 +0530

    contained the sourcefinder to the art-music-etc channel only

--- cogs/source.py
+++ cogs/source.py
@@ -16,28 +16,29 @@ class Source(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        if "source" not in message.content.lower():
-            for attachment in message.attachments:
-                img_data = requests.get(attachment.url).content
+        if message.channel.id == 413449617401708555:
+            if "source" not in message.content.lower():
+                for attachment in message.attachments:
+                    img_data = requests.get(attachment.url).content
 
-                with open('data/temp.jpg', 'wb') as handler:
-                    handler.write(img_data)
+                    with open('data/temp.jpg', 'wb') as handler:
+                        handler.write(img_data)
 
-                filtered_results = saucenao.check_file(file_name="temp.jpg")
-                if len(filtered_results) != 0:
-                    for result in filtered_results:
-                        try:
-                            unparsed = result['data']['content'][0].split('\n')[0].split(" ")
-                            for count, word in enumerate(unparsed):
-                                if word == "Pixiv":
-                                    if unparsed[count + 1] == "ID:":
-                                        pixiv_id = unparsed[count + 2]
-                                    elif unparsed[count + 1][0] == "#":
-                                        pixiv_id = unparsed[count + 1][1:]
-                            await message.channel.send(f"Source: <https://www.pixiv.net/en/artworks/{pixiv_id}>")
-                            break
-                        except Exception as e:
-                            print(e)
+                    filtered_results = saucenao.check_file(file_name="temp.jpg")
+                    if len(filtered_results) != 0:
+                        for result in filtered_results:
+                            try:
+                                unparsed = result['data']['content'][0].split('\n')[0].split(" ")
+                                for count, word in enumerate(unparsed):
+                                    if word == "Pixiv":
+                                        if unparsed[count + 1] == "ID:":
+                                            pixiv_id = unparsed[count + 2]
+                                        elif unparsed[count + 1][0] == "#":
+                                            pixiv_id = unparsed[count + 1][1:]
+                                await message.channel.send(f"Source: <https://www.pixiv.net/en/artworks/{pixiv_id}>")
+                                break
+                            except Exception as e:
+                                print(e)
 
 
 def setup(bot):
