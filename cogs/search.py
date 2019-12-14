1e7a29ddb3b284dbbe239c83d338dcb542720234
Thennal <thennal10@gmail.com>
2019-12-14 14:15:00 +0530

    added vglist search command

--- cogs/search.py
+++ cogs/search.py
@@ -1,9 +1,12 @@
+import os
 import requests
 import wikipedia
 from discord.ext import commands
 from jikanpy import Jikan
 
 jikan = Jikan()
+vglist_token = os.environ['VGLIST_TOKEN']
+vglist_email = os.environ['VGLIST_EMAIL']
 
 class Search(commands.Cog):
 
@@ -38,7 +41,8 @@ class Search(commands.Cog):
 
     @anisearch.error
     async def anisearch_error(self, ctx, error):
-        await ctx.send("Usage: ``!anime [search]``")
+        print(error)
+        await ctx.send("Usage: ``$anime [search]``")
 
 
     @commands.command(name='manga', help='Pulls up the mal and anilist of a given manga/ln')
@@ -70,7 +74,45 @@ class Search(commands.Cog):
 
     @mangasearch.error
     async def mangasearch_error(self, ctx, error):
-        await ctx.send("Usage: ``!manga [search]``")
+        print(error)
+        await ctx.send("Usage: ``$manga [search]``")
+
+
+    @commands.command(name='game', help='Pulls up the vglist link for a given game')
+    async def gamesearch(self, ctx, *, search):
+        query = '''
+                query($query: String!) {
+                  gameSearch(query: $query) {
+                    nodes {
+                      id
+                      name
+                      coverUrl
+                    }
+                  }
+                }
+                '''
+        variables = {'query': search}
+        headers = {
+            "User-Agent": "Yikes",
+            "X-User-Token": vglist_token,
+            "X-User-Email": vglist_email,
+            "Content-Type": "application/json",
+            "Accept": "*/*"
+        }
+        url = 'https://vglist.co/graphql'
+
+        response = requests.post(url, json={'query': query, 'variables': variables}, headers=headers).json()
+
+        results = response['data']['gameSearch']['nodes']
+        if len(results) == 0:
+            await ctx.send(f"Game not found.")
+        else:
+            await ctx.send(f"https://vglist.co/games/{results[0]['id']}")
+
+    @gamesearch.error
+    async def gamesearch_error(self, ctx, error):
+        print(error)
+        await ctx.send("Usage: ``$game [search]``")
 
 
     @commands.command(name='wiki', help='Pulls up the wikipedia link for a given search')
@@ -82,7 +124,8 @@ class Search(commands.Cog):
 
     @wikisearch.error
     async def wikisearch_error(self, ctx, error):
-        await ctx.send("Usage: ``!wiki [search]``")
+        print(error)
+        await ctx.send("Usage: ``$wiki [search]``")
 
 def setup(bot):
     bot.add_cog(Search(bot))
