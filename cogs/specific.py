30b7834aac6293bf897e06e03a0b7b1af9412c03
Thennal <thennal10@gmail.com>
2020-04-27 17:52:16 +0530

    modified the echo command to not get stuck in an infinite loop replying to itself

--- cogs/specific.py
+++ cogs/specific.py
@@ -8,7 +8,7 @@ class Specific(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        if message.channel.id == 635538705389584425:
+        if message.channel.id == 635538705389584425 and message.author.id == 293716762636320768:
             try:
                 msg = message.content.split(" ")
                 output_channel = self.bot.get_channel(int(msg[0]))
@@ -17,15 +17,6 @@ class Specific(commands.Cog):
                 await message.channel.send("The first word of the message should be the channel ID.")
             except AttributeError:
                 await message.channel.send("Can't find a channel with the given ID.")
-        elif message.author.id == 453714878645927936 and message.content == "<:chibiaqua:611916034290614283>":
-            posts = await message.channel.history(limit=2).flatten()
-            delta = (message.created_at - posts[1].created_at)
-            seconds = delta.seconds + (delta.days * 86400)
-            hour = round(seconds / 3600)
-            if hour > 1:
-                await message.channel.send(f"Fire, posting a singular <:chibiaqua:611916034290614283> {hour} hours after"
-                                           f" the conversation ended does not count as contributing to it.")
-
 
 def setup(bot):
     bot.add_cog(Specific(bot))
