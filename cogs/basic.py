2d633a2c96187689fef73d632a977fa2b26f2080
thennal <thennal10@gmail.com>
2021-01-08 19:29:05 +0530

    removed dumb server check and added owner checks
    also reformatted a lot of shit. what is good code

--- cogs/basic.py
+++ cogs/basic.py
@@ -5,39 +5,20 @@ import random
 import psycopg2
 import discord
 from wordcloud import WordCloud
-from wordcloud import STOPWORDS
 from discord.ext import commands
 
-MIA = 405533644250152960
 DATABASE_URL = os.environ['DATABASE_URL']
 WORDS = json.load(open('data/fuckrai.json'))
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
-def leaderboard_name(l):
-    name = ""
-    for count, element in enumerate(l):
-        name += element.capitalize()
-        if count != len(l) - 1:
-            name += "/"
-    return name
-
-async def check_server(ctx):
-    if MIA == ctx.guild.id:
-        return True
-    else:
-        await ctx.send("This command is currently disabled for this server.")
-
 
 class Basic(commands.Cog):
-    """BasicCog"""
 
     def __init__(self, bot):
         self.bot = bot
 
-
     @commands.command(name='strokify', help='TuRnS gIvEn InPuT tO tHiS')
     async def strokify(self, ctx, *, input: str):
-
         iscap = False
         outputmsg = list(input)
         for count, character in enumerate(outputmsg):
@@ -58,7 +39,9 @@ class Basic(commands.Cog):
 
         await ctx.send(f"bout {num}/10")
 
-    @commands.command(name='leaderboard', help='Creates a leaderboard based on a given word/phrase', usage='$leaderboard [no of messages] [phrase1] + [phrase2]...')
+    @commands.command(name='leaderboard',
+                      help='Creates a leaderboard based on a given word/phrase',
+                      usage='[no of messages] [phrase1] + [phrase2]...')
     async def word_leaderboard(self, ctx, msglimit: int, *, actmsg: str):
 
         searchwords = actmsg.split(" + ")
@@ -80,7 +63,7 @@ class Basic(commands.Cog):
                             if found == 0:
                                 peeps.append([msg.author.name, 1])
             print(chan)
-        peeps.sort(key=lambda x:x[1], reverse=True)
+        peeps.sort(key=lambda x: x[1], reverse=True)
 
         leaderboard = f"**The {leaderboard_name(searchwords)} Leaderboard**```\nRank  | Name\n\n"
         for count, peep in enumerate(peeps[:10]):
@@ -88,13 +71,15 @@ class Basic(commands.Cog):
         leaderboard += "```"
         await ctx.send(leaderboard)
 
-    @commands.command(name='wordcloud', aliases=['wc'], help='Creates a wordcloud based on the mentioned user')
-    async def wordcloud(self, ctx, member: discord.Member, channel: discord.TextChannel, lookup_num: int = 1000):
-        wordstr = "" # Somewhere to store the words
+    @commands.command(name='wordcloud',
+                      aliases=['wc'],
+                      help='Creates a wordcloud based on the mentioned user.')
+    async def wordcloud(self, ctx, member: discord.Member, channel: discord.TextChannel, number_of_messages: int = 1000):
+        wordstr = ""  # Somewhere to store the words
 
         perm = channel.permissions_for(ctx.guild.me)
         if perm.read_message_history:
-            async for msg in channel.history(limit=lookup_num):
+            async for msg in channel.history(limit=number_of_messages):
                 if msg.author == member:
                     wordstr += " " + msg.content.capitalize()
 
@@ -106,7 +91,7 @@ class Basic(commands.Cog):
         await ctx.send(file=discord.File('data/wordcloud.png'))
 
     @commands.command(name='peachlator', help='What it says on the tin')
-    async def peachlator(self, ctx, *, inp: str):
+    async def peachlator(self, ctx, *, input: str):
         # get the table
         cur = conn.cursor()
         sql = """SELECT * FROM peachdict;"""
@@ -115,11 +100,10 @@ class Basic(commands.Cog):
         cur.close()
 
         # made it into a list so I don't have to worry about differing word lengths
-        new_message = ["Translation:"] + inp.split()
+        new_message = ["Translation:"] + input.split()
 
         # new dict with keys as a list of words
         flattened_dict = {tuple(k[0].split()): k[1] for k in table}
-
 
         for key in flattened_dict:
             # indicates the point we're at with the given key
@@ -161,56 +145,57 @@ class Basic(commands.Cog):
 
         await ctx.send(" ".join(new_message))
 
-    @commands.command(name='update_peachlator', help='Update the peachlator', usage='$update_peachlator [word] - [translation]')
+    @commands.command(name='update_peachlator',
+                      help='Update the peachlator. Premed only.',
+                      usage='$update_peachlator [word] - [translation]')
+    @commands.is_owner()
     async def update_peachlator(self, ctx, *, inp: str):
-        if_MIA = await check_server(ctx)
-        if if_MIA:
-            data = [word.strip() for word in inp.split("-")]
+        data = [word.strip() for word in inp.split("-")]
 
-            if len(data) > 2 or len(data) < 2:
-                await ctx.send("Usage: ``$update_peachlator [word] - [translation]``")
-                return
+        if len(data) > 2 or len(data) < 2:
+            await ctx.send("Usage: ``$update_peachlator [word] - [translation]``")
+            return
 
-            sql = """INSERT INTO peachdict (input, output) VALUES (%s, %s);"""
+        sql = """INSERT INTO peachdict (input, output) VALUES (%s, %s);"""
 
-            cur = conn.cursor()
-            try:
-                cur.execute(sql, data)
-                conn.commit()
-                cur.close()
-                await ctx.send("Updated!")
-            except:
-                conn.rollback()
-                conn.commit()
-                cur.close()
-                await ctx.send("Translation already exists, or you fucking broke the bot. Congrats, asshole.")
-
-    @commands.command(name='remove_peachlator', help='Removes a translation')
-    async def remove_peachlator(self, ctx, *, key: str):
-        if_MIA = await check_server(ctx)
-        if if_MIA:
-            # Even more SQL
-            cur = conn.cursor()
-            sql = """SELECT input, output FROM peachdict;"""
-            cur.execute(sql)
-            row = cur.fetchone()
-            while row is not None:
-                if row[0] == key:
-                    sql = f"""DELETE FROM peachdict WHERE input='{key}';"""
-                    cur.execute(sql)
-                    conn.commit()
-                    await ctx.send("Removal successful.")
-                    break
-                row = cur.fetchone()
-            else:
-                await ctx.send("Translation doesn't exist.")
-
+        cur = conn.cursor()
+        try:
+            cur.execute(sql, data)
+            conn.commit()
             cur.close()
+            await ctx.send("Updated!")
+        except Exception as e:
+            print(e)
+            conn.rollback()
+            conn.commit()
+            cur.close()
+            await ctx.send("Translation already exists, or you fucking broke the bot. Congrats, asshole.")
+
+    @commands.command(name='remove_peachlator', help='Removes a translation. Premed only.')
+    @commands.is_owner()
+    async def remove_peachlator(self, ctx, *, key: str):
+        # Even more SQL
+        cur = conn.cursor()
+        sql = """SELECT input, output FROM peachdict;"""
+        cur.execute(sql)
+        row = cur.fetchone()
+        while row is not None:
+            if row[0] == key:
+                sql = f"""DELETE FROM peachdict WHERE input='{key}';"""
+                cur.execute(sql)
+                conn.commit()
+                await ctx.send("Removal successful.")
+                break
+            row = cur.fetchone()
+        else:
+            await ctx.send("Translation doesn't exist.")
+
+        cur.close()
 
     @commands.command(name='madlibs', helps="Replaces word types encased in square brackets with random words")
-    async def madlibs(self, ctx, *, msg: str):
+    async def madlibs(self, ctx, *, phrase: str):
         # A dict to convert word type aliases to the actual word type
-        wordalias = {'noun' : 'noun',
+        wordalias = {'noun': 'noun',
                      'n': 'noun',
                      'verb': 'verb',
                      'v': 'verb',
@@ -221,25 +206,35 @@ class Basic(commands.Cog):
 
         # Regex to detect anything in between square brackets
         pattern = re.compile('\[(.*?)\]')
-        if not pattern.findall(msg):
+        if not pattern.findall(phrase):
             return await ctx.send("No words to swap given. Enclose a valid word type in square brackets, "
                                   "like ``[noun]``.")
 
         output = ""
         prev = 0
-        iterator = pattern.finditer(msg)
+        iterator = pattern.finditer(phrase)
 
         try:
             for match in iterator:
                 # good luck trying to figure this out in three years
-                output += msg[prev:match.span()[0]] + random.choice(WORDS[(wordalias[match.group()[1:-1]])])
+                output += phrase[prev:match.span()[0]] + random.choice(WORDS[(wordalias[match.group()[1:-1]])])
                 prev = match.span()[1]
-            output += msg[prev:]
+            output += phrase[prev:]
 
             await ctx.send(output)
         except KeyError as e:
             await ctx.send(f"No idea what ``{e.args[0]}`` is. Valid word types are ``noun`` (``n``), ``verb`` (``v``), "
                            f"``adjective`` (``adj``), and ``adverb`` (``adv``).")
+
+
+def leaderboard_name(l):
+    name = ""
+    for count, element in enumerate(l):
+        name += element.capitalize()
+        if count != len(l) - 1:
+            name += "/"
+    return name
+
 
 def setup(bot):
     bot.add_cog(Basic(bot))
