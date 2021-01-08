0d47c76d02581c2bbd8035d8f657e5b1d6dbc677
thennal <thennal10@gmail.com>
2021-01-08 19:40:50 +0530

    removed deprecated individual error decorators
    minor reformatting

--- cogs/search.py
+++ cogs/search.py
@@ -3,9 +3,9 @@ import requests
 from discord import Embed
 from discord.ext import commands
 
-
 vglist_token = os.environ['VGLIST_TOKEN']
 vglist_email = os.environ['VGLIST_EMAIL']
+
 
 class Search(commands.Cog):
 
@@ -34,7 +34,6 @@ class Search(commands.Cog):
         else:
             await ctx.reply(f"Anilist URL not found")
 
-
     @commands.command(name='manga', help='Pulls up the mal and anilist of a given manga/ln')
     async def mangasearch(self, ctx, *, search):
         query = '''
@@ -57,12 +56,6 @@ class Search(commands.Cog):
             await ctx.reply(f"https://anilist.co/manga/{manga_id}")
         else:
             await ctx.reply(f"Anilist URL not found")
-
-    @mangasearch.error
-    async def mangasearch_error(self, ctx, error):
-        print(error)
-        await ctx.reply("Usage: ``$manga [search]``")
-
 
     @commands.command(name='game', help='Pulls up the vglist link for a given game')
     async def gamesearch(self, ctx, *, search):
@@ -97,12 +90,6 @@ class Search(commands.Cog):
                 output += f"\nhttps://store.steampowered.com/app/{results[0]['steamAppIds'][0]}"
             await ctx.reply(output)
 
-    @gamesearch.error
-    async def gamesearch_error(self, ctx, error):
-        print(error)
-        await ctx.reply("Usage: ``$game [search]``")
-
-
     @commands.command(name='wiki', help='Pulls up the wikipedia link for a given search')
     async def wikisearch(self, ctx, *, search):
         params = {
@@ -124,12 +111,6 @@ class Search(commands.Cog):
                     return await ctx.reply(result[r]['fullurl'])
         except KeyError:
             await ctx.reply(f"Nothing found for ``{search}``.")
-
-    @wikisearch.error
-    async def wikisearch_error(self, ctx, error):
-        print(error)
-        await ctx.reply("Usage: ``$wiki [search]``")
-
 
     @commands.command(name='urban', help='Pulls up the urban dictionary entry for a given search')
     async def urban(self, ctx, *, search):
@@ -155,30 +136,20 @@ class Search(commands.Cog):
         except IndexError:
             await ctx.reply(f"Nothing found for ``{search}``.")
 
-    @urban.error
-    async def urban_error(self, ctx, error):
-        print(error)
-        await ctx.reply("Usage: ``$urban [search]``")
-
-
     @commands.command(name='rt', help='Pulls up the rotten tomatoes entry for a given search')
     async def rt(self, ctx, *, search):
-        rq = requests.get("https://www.rottentomatoes.com/api/private/v2.0/search", params={"q":search})
+        rq = requests.get("https://www.rottentomatoes.com/api/private/v2.0/search", params={"q": search})
         try:
             result_url = rq.json()['movies'][0]['url']
             await ctx.reply("https://www.rottentomatoes.com" + result_url)
         except IndexError:
             await ctx.reply(f"Nothing found for ``{search}``.")
 
-    @rt.error
-    async def rt_error(self, ctx, error):
-        print(error)
-        await ctx.reply("Usage: ``$rt [search]``")
-
 
 # just removes square brackets, used for $urban
 def parse(word):
-    return "".join([ch for ch in word if ch not in ['[',']']])
+    return "".join([ch for ch in word if ch not in ['[', ']']])
+
 
 def setup(bot):
     bot.add_cog(Search(bot))
