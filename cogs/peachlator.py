5758bdac57b605cb74a56ba47de7c0ab46431c59
thennal <thennal10@gmail.com>
2021-01-12 11:40:00 +0530

    seperated peachlator into its own cog

---
+++ cogs/peachlator.py
@@ -0,0 +1,118 @@
+import os
+import psycopg2
+from discord.ext import commands
+
+DATABASE_URL = os.environ['DATABASE_URL']
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+
+
+class Peachlator(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name='peachlator', help='What it says on the tin.')
+    async def peachlator(self, ctx, *, input: str):
+        # get the table
+        cur = conn.cursor()
+        sql = """SELECT * FROM peachdict;"""
+        cur.execute(sql)
+        table = cur.fetchall()
+        cur.close()
+
+        # made it into a list so I don't have to worry about differing word lengths
+        new_message = ["Translation:"] + input.split()
+
+        # new dict with keys as a list of words
+        flattened_dict = {tuple(k[0].split()): k[1] for k in table}
+
+        for key in flattened_dict:
+            # indicates the point we're at with the given key
+            i = 0
+            caps = False
+            allcaps = False
+            for idx, word in enumerate(new_message):
+                if (key[i] == word) or (key[i] == word.lower()):
+
+                    # check if the word is capitalized or in allcaps
+                    if key[i].capitalize() == word:
+                        caps = True
+                    if key[i].upper() == word:
+                        allcaps = True
+
+                    i += 1
+                    # if we find a valid word
+                    if i == len(key):
+
+                        if caps:
+                            result = flattened_dict[key].capitalize()
+                        elif allcaps:
+                            result = flattened_dict[key].upper()
+                        else:
+                            result = flattened_dict[key]
+
+                        new_message[idx] = result
+                        # remove leftover words
+                        for r in range(1, i):
+                            new_message.pop(idx - r)
+
+                        i = 0
+
+                else:
+                    # so that it's a continuous word and not like, splintered across a sentence
+                    i = 0
+                    caps = False
+                    allcaps = False
+
+        await ctx.send(" ".join(new_message))
+
+    @commands.command(name='update_peachlator',
+                      help='Update the peachlator. Premed only.',
+                      usage='$update_peachlator [word] - [translation]')
+    @commands.is_owner()
+    async def update_peachlator(self, ctx, *, inp: str):
+        data = [word.strip() for word in inp.split("-")]
+
+        if len(data) > 2 or len(data) < 2:
+            await ctx.send("Usage: ``$update_peachlator [word] - [translation]``")
+            return
+
+        sql = """INSERT INTO peachdict (input, output) VALUES (%s, %s);"""
+
+        cur = conn.cursor()
+        try:
+            cur.execute(sql, data)
+            conn.commit()
+            cur.close()
+            await ctx.send("Updated!")
+        except Exception as e:
+            print(e)
+            conn.rollback()
+            conn.commit()
+            cur.close()
+            await ctx.send("Translation already exists, or you fucking broke the bot. Congrats, asshole.")
+
+    @commands.command(name='remove_peachlator', help='Removes a translation. Premed only.')
+    @commands.is_owner()
+    async def remove_peachlator(self, ctx, *, key: str):
+        # Even more SQL
+        cur = conn.cursor()
+        sql = """SELECT input, output FROM peachdict;"""
+        cur.execute(sql)
+        row = cur.fetchone()
+        while row is not None:
+            if row[0] == key:
+                sql = f"""DELETE FROM peachdict WHERE input='{key}';"""
+                cur.execute(sql)
+                conn.commit()
+                await ctx.send("Removal successful.")
+                break
+            row = cur.fetchone()
+        else:
+            await ctx.send("Translation doesn't exist.")
+
+        cur.close()
+
+
+def setup(bot):
+    bot.add_cog(Peachlator(bot))
